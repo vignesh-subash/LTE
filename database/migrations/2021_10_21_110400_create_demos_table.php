83746c8d86434943f7cc4fb6b66f529609476508
vignesh Subsah <vignesh-subash@users.noreply.github.com>
2021-10-26 17:49:50 +0530

    Update 2021_10_21_110400_create_demos_table.php
    Update Demo Table

--- database/migrations/2021_10_21_110400_create_demos_table.php
+++ database/migrations/2021_10_21_110400_create_demos_table.php
@@ -15,9 +15,35 @@ class CreateDemosTable extends Migration
     {
         Schema::create('demos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('prefix')->nullable;
-            $table->string('salutation1');
-            $table->string('contactPerson1')
+            $table->string('prefix');
+            $table->string('companyName');
+            $table->string('salutation1')->nullable;
+            $table->string('contactPerson1')->nullable;
+            $table->string('designation1')->nullable;
+            $table->string('salutation2')->nullable;
+            $table->string('contactPerson2')->nullable;
+            $table->string('designation2')->nullable;
+            $table->string('add1');
+            $table->string('add2')->nullable;
+            $table->string('locality');
+            $table->string('city');
+            $table->char('pincode', 6);
+            $table->string('state');
+            $table->string('country');
+            $table->string('phone1', 15)->nullable;
+            $table->string('phone2', 15)->nullable;
+            $table->string('phone3', 15)->nullable;
+            $table->string('mobile1', 14)->nullable;
+            $table->string('mobile2', 14)->nullable;
+            $table->string('mobile3', 14)->nullable;
+            $table->string('fax', 16)->nullable;
+            $table->string('email1')->nullable;
+            $table->string('email2')->nullable;
+            $table->string('website1')->nullable;
+            $table->string('website2')->nullable;
+            $table->date('birthday')->nullable;
+            $table->date('anniversary')->nullable;
+      
             $table->timestamps();
             $table->softDeletes();
         });
